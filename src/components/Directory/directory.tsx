d4131485508beea1be4908b5ce09664d8feecd58
Brenden Clerget <brenden.clerget@robotsandpencils.com>
2022-10-25 23:40:21 -0700

    Layout styling/fixes
    Fixing up layout styling and making it completely responsive

--- src/components/Directory/directory.tsx
+++ src/components/Directory/directory.tsx
@@ -3,12 +3,47 @@ import './directory.scss';
 const Directory = () => {
   return (
     <div className='directory'>
-      <h2>Browse Pokémon</h2>
+      <h2 className='directory-header'>Browse Pokémon</h2>
 
       <div id='pokegrid'>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1asfsadf</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
+          <div className='pokecard-name'>Bulbasaur</div>
+          <div className='pokecard-types'>
+            <div className='pokecard-types-pill'>fighting</div>
+            <div className='pokecard-types-pill'>fighting</div>
+          </div>
+        </div>
+        <div className='pokecard'>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
+          <div className='pokecard-name'>Bulbasaur</div>
+          <div className='pokecard-types'>
+            <div className='pokecard-types-pill'>Fighting</div>
+            <div className='pokecard-types-pill'>Poison</div>
+          </div>
+        </div>
+        <div className='pokecard'>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
+          <div className='pokecard-name'>Bulbasaur</div>
+          <div className='pokecard-types'>
+            <div className='pokecard-types-pill'>Grass</div>
+            <div className='pokecard-types-pill'>Lightning</div>
+          </div>
+        </div>
+        <div className='pokecard'>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
@@ -16,8 +51,10 @@ const Directory = () => {
           </div>
         </div>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
@@ -25,8 +62,10 @@ const Directory = () => {
           </div>
         </div>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
@@ -34,8 +73,10 @@ const Directory = () => {
           </div>
         </div>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
@@ -43,8 +84,10 @@ const Directory = () => {
           </div>
         </div>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
@@ -52,8 +95,32 @@ const Directory = () => {
           </div>
         </div>
         <div className='pokecard'>
-          <div className='pokecard-id'>#1</div>
-          <div className='pokecard-image'>Image</div>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
+          <div className='pokecard-name'>Bulbasaur</div>
+          <div className='pokecard-types'>
+            <div className='pokecard-types-pill'>Grass</div>
+            <div className='pokecard-types-pill'>Poison</div>
+          </div>
+        </div>
+        <div className='pokecard'>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
+          <div className='pokecard-name'>Bulbasaur</div>
+          <div className='pokecard-types'>
+            <div className='pokecard-types-pill'>Grass</div>
+            <div className='pokecard-types-pill'>Poison</div>
+          </div>
+        </div>
+        <div className='pokecard'>
+          <div className='pokecard-number'>#1</div>
+          <div className='pokecard-image'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' />
+          </div>
           <div className='pokecard-name'>Bulbasaur</div>
           <div className='pokecard-types'>
             <div className='pokecard-types-pill'>Grass</div>
